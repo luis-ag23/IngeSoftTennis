f5cac91cadd5a0b9798f64120f0a0a296a8d58ca
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2026-03-02 22:44:36 -0400

    feat: añadiendo el html para el juego del tennis

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,32 @@
-import sumar from "./sumador";
+import Tennis from "./tennis.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const tennis = new Tennis();
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+const scoreP1 = document.querySelector(".scoreP1");
+const scoreP2 = document.querySelector(".scoreP2");
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+const btnP1 = document.querySelector(".btn-p1");
+const btnP2 = document.querySelector(".btn-p2");
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+function updateUI() {
+  const result = tennis.score();
+
+  if (result.includes("-")) {
+    const [p1, p2] = result.split(" - ");
+    scoreP1.textContent = p1;
+    scoreP2.textContent = p2;
+  } else {
+    scoreP1.textContent = result;
+    scoreP2.textContent = result;
+  }
+}
+
+btnP1.addEventListener("click", () => {
+  tennis.player1Scores();
+  updateUI();
+});
+
+btnP2.addEventListener("click", () => {
+  tennis.player2Scores();
+  updateUI();
 });
